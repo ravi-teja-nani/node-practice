3d0fc0dbb807259b3ca7066932ad8c939b1c81df
ravi-teja-nani <ravi003teja@hotmail.com>
2020-07-26 01:18:21 -0700

    lesson2 about server module

---
+++ lession_02/server.js
@@ -0,0 +1,45 @@
+let http = require('http');
+let fs = require('fs')
+let server = http.createServer((req, res)=>{
+    console.log(req.url, req.method);
+    //writing response
+ res.setHeader('Content-Type', 'text/html');
+ 
+ let path = './views/';
+ switch(req.url){
+     case '/' : 
+        path += 'index.html';
+        res.statusCode = 200;
+        break;
+     case '/about' : 
+        path += 'about.html';
+        res.statusCode = 200;
+        break;
+     case '/about-me' : 
+        res.statusCode = 301;
+        res.setHeader('Location', '/about');
+        res.end();
+        break;        
+     default:
+         path += '404.html' 
+         res.statusCode = 400;
+
+
+
+ }
+
+   fs.readFile(path, (err, data)=>{
+       if(!err){
+           res.write(data);
+           res.end() //res.end(data)
+       }else{
+           console.log(err);
+           res.end();
+       }
+   })
+
+})
+
+server.listen(3000, 'localhost', ()=>{
+    console.log("listening for request on port 3000")
+})
